78406069b8b488794b5ecebce2fe95f4f5c57700
Luis Angel <angelfgdeveloper@gmail.com>
2024-01-19 12:49:40 -0600

    Uso de Nodemon

---
+++ README.md
@@ -0,0 +1,6 @@
+# 02 Bases de Node
+
+Ejecutar en DEV
+```
+npm run dev
+```
--- src/js-foundation/01-template.js
+++ src/js-foundation/01-template.js
@@ -3,6 +3,7 @@ const emailTemplate = `
   <div>
     <h1>Hola, {{name}}</h1>
     <p>Gracias por su orden.</p>
+    <p>Order ID: {{orderId}}</p>
   </div>
 `;
 
