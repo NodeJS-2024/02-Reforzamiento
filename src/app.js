a2f4e3a1656eaa2361ad8fd42dce7ac6df786f19
Luis Angel <angelfgdeveloper@gmail.com>
2024-01-19 13:17:51 -0600

    Uso de callbacks

--- src/app.js
+++ src/app.js
@@ -3,9 +3,32 @@
 
 const { emailTemplate } = require('./js-foundation/01-template');
 require('./js-foundation/02-destructuring');
+const { getUserById } = require('./js-foundation/03-callbacks');
 
 
 // console.log(emailTemplate);
+
+const id = 1;
+
+getUserById(id, function( error, user ) {
+  if ( error ) {
+    throw new Error(error);
+  }
+
+  console.log({ user });
+
+  // callback hell no recomentado
+  // getUserById(2, function( error, user2 ) {
+  //   if ( error ) {
+  //     throw new Error(error);
+  //   }  
+  
+  //   console.log({user, user2});
+  
+  // });
+  
+
+});
 
 
 
