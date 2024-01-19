96bc40eaea05d31b81c8d6d6dff5c7f1a44d6bb8
Luis Angel <angelfgdeveloper@gmail.com>
2024-01-19 16:44:34 -0600

    Axios como cliente para peticiones HTTP

--- src/plugins/http-client.plugin.js
+++ src/plugins/http-client.plugin.js
@@ -1,10 +1,17 @@
 
+const axios = require('axios');
+
+// const buildHttpClient = (headers) => {}
+
 // Patron adaptador
 const httpClientPlugin = {
 
   get: async(url) => {
-    const response = await fetch(url);
-    return await response.json();
+    // const response = await fetch(url);
+    // return await response.json();
+
+    const { data } = await axios.get(url);
+    return data;
   },
   post: async(url, body) => {},
   put: async(url, id, body) => {},
