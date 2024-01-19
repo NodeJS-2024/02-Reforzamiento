4ee95d3102eb048d4ff8ed3abc91522bc9568f6b
Luis Angel <angelfgdeveloper@gmail.com>
2024-01-19 13:29:31 -0600

    Uso de arrow functions

--- src/app.js
+++ src/app.js
@@ -3,33 +3,44 @@
 
 const { emailTemplate } = require('./js-foundation/01-template');
 require('./js-foundation/02-destructuring');
-const { getUserById } = require('./js-foundation/03-callbacks');
+// const { getUserById } = require('./js-foundation/03-callbacks');
+const { getUserById } = require('./js-foundation/04-arrow');
 
 
 // console.log(emailTemplate);
 
 const id = 1;
 
-getUserById(id, function( error, user ) {
+// getUserById(id, function( error, user ) {
+//   if ( error ) {
+//     throw new Error(error);
+//   }
+
+//   // console.log({ user });
+
+//   // callback hell no recomentado
+//   // getUserById(2, function( error, user2 ) {
+//   //   if ( error ) {
+//   //     throw new Error(error);
+//   //   }  
+  
+//   //   console.log({user, user2});
+  
+//   // });
+  
+
+// });
+
+// Convertido a arrow fuctions
+getUserById(id, ( error, user ) => {
+
   if ( error ) {
     throw new Error(error);
   }
 
   console.log({ user });
 
-  // callback hell no recomentado
-  // getUserById(2, function( error, user2 ) {
-  //   if ( error ) {
-  //     throw new Error(error);
-  //   }  
-  
-  //   console.log({user, user2});
-  
-  // });
-  
-
 });
-
 
 
 
