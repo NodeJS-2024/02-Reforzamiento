3d66dad53021c9248649836df0757676b51dfa43
Luis Angel <angelfgdeveloper@gmail.com>
2024-01-23 15:17:52 -0600

    Configurando winston

--- src/plugins/logger.plugin.js
+++ src/plugins/logger.plugin.js
@@ -1,9 +1,15 @@
 const winston = require('winston');
+const { combine, timestamp, json } = winston.format;
+
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  // format: winston.format.json(),
   // defaultMeta: { service: 'user-service' },
+  format: combine(
+    timestamp(),
+    json(),
+  ),
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
@@ -23,6 +29,13 @@ module.exports = function buildLogger(service) {
   return {
     log: (message) => {
       logger.log('info', { message, service });
+    },
+    error: (message) => {
+      logger.error('error', { 
+        message, 
+        service,
+        // at: new Date().toISOString()
+      });
     }
   }
   
